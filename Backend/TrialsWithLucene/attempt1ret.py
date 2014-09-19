349831a99ff65288e8c1920ff78bcda9ac3056b9
Nitin Jamadagni <nitin.jamadagni@gmail.com>
2014-09-20 02:33:53 +0530

    Lucene Store And Search Basic

---
+++ Backend/TrialsWithLucene/attempt1ret.py
@@ -0,0 +1,40 @@
+import lucene
+import string
+import simplejson as json
+
+INDEX_DIR="IndexFiles.index"
+MAX_RESULTS=1
+
+def  search(searcher,analyzer):
+	print 
+	query=lucene.QueryParser(lucene.Version.LUCENE_CURRENT,"name",analyzer).parse("nitin")
+	hits=searcher.search(query,MAX_RESULTS).scoreDocs
+	for hit in hits:
+		doc=searcher.doc(hit.doc)
+		print "name is:nitin and data is:",doc
+
+	print 
+	query=lucene.QueryParser(lucene.Version.LUCENE_CURRENT,"name",analyzer).parse("nitin2")
+	hits=searcher.search(query,MAX_RESULTS).scoreDocs
+	for hit in hits:
+		doc=searcher.doc(hit.doc)
+		print "name is:nitin2 and data is:",doc
+
+	print 
+	print "Successfully retrieved documents....." 
+
+
+
+
+
+def main1():
+	print "retrieve and display files......"
+	direc=lucene.SimpleFSDirectory(lucene.File(INDEX_DIR))
+	analyzer=lucene.StandardAnalyzer(lucene.Version.LUCENE_CURRENT)
+	searcher=lucene.IndexSearcher(direc)
+	search(searcher,analyzer)
+
+if __name__ == '__main__':
+	lucene.initVM()
+	print "Started lucene with version number :",lucene.VERSION
+	main1()
