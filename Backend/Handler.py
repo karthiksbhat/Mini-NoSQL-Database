315b74ea01ad9fd4c73b94bcc1e37a47282e4695
Nitin Jamadagni <nitin.jamadagni@gmail.com>
2014-09-26 00:20:46 +0530

    Updated search for any-value-search

--- Backend/Handler.py
+++ Backend/Handler.py
@@ -1,9 +1,7 @@
 import lucene
-import string
 import simplejson as json
 import os
 import csv
-from lucene import Document
 
 INDEX_DIR_DEFAULT="IndexFiles.index"     #default value
 primary_keys_map={}
@@ -15,9 +13,35 @@ def store(collection_name,data):
 	else:
 		INDEX_DIR=INDEX_DIR_DEFAULT	
 	print "started indexing input data......"
-	#setting writer configurations
+	
+	#extracting values
+	try:
+		contents=json.loads(data)
+	except:
+		return 100
+
+
 	direc=lucene.SimpleFSDirectory(lucene.File(INDEX_DIR))
 	analyzer=lucene.StandardAnalyzer(lucene.Version.LUCENE_CURRENT)
+	
+
+	#checking for existance of record with same primary_key set
+	try:
+		searcher=lucene.IndexSearcher(direc)
+		query=lucene.BooleanQuery()
+		for key in primary_keys_map[INDEX_DIR]:
+			temp=lucene.QueryParser(lucene.Version.LUCENE_CURRENT,key,analyzer).parse(contents[key])
+			query.add(lucene.BooleanClause(temp,lucene.BooleanClause.Occur.MUST))
+		hits=searcher.search(query,MAX_RESULTS).scoreDocs
+		if len(hits) > 0:
+			return 106
+	except:
+		pass 	 
+	
+	
+
+
+	#setting writer configurations
 	config=lucene.IndexWriterConfig(lucene.Version.LUCENE_CURRENT,analyzer)
 	config.setOpenMode(lucene.IndexWriterConfig.OpenMode.CREATE_OR_APPEND)
 	writer=lucene.IndexWriter(direc,config)
@@ -26,10 +50,7 @@ def store(collection_name,data):
 	#field_type.setIndexed(True)
 	#field_type.setStored(False)
 	#field_type.setTokenized(False)
-	try:
-		contents=json.loads(data)
-	except:
-		return 100
+	
 	try:
 		doc=lucene.Document()
 		#index files wrt primary key
@@ -49,29 +70,43 @@ def store(collection_name,data):
 	except:
 		return 102
 
-
-def  search(collection_name,primary_keyvalue_pairs):
+def  search(collection_name,tofind):
 	if collection_name!="DEFAULT":
 		INDEX_DIR=collection_name
 	else:
 		INDEX_DIR=INDEX_DIR_DEFAULT
-		
+	try:
+		tofind_keyvalue_pairs=json.loads(tofind)
+	except:
+		return 100	
 	direc=lucene.SimpleFSDirectory(lucene.File(INDEX_DIR))
 	analyzer=lucene.StandardAnalyzer(lucene.Version.LUCENE_CURRENT)
 	try:	
 		searcher=lucene.IndexSearcher(direc)
 	except:
-		return 105	
-	query=lucene.BooleanQuery()
-	for key in primary_keyvalue_pairs.keys():
-		temp=lucene.QueryParser(lucene.Version.LUCENE_CURRENT,key,analyzer).parse(primary_keyvalue_pairs[key])
-		query.add(lucene.BooleanClause(temp,lucene.BooleanClause.Occur.MUST))
-	hits=searcher.search(query,MAX_RESULTS).scoreDocs
-	
+		return 105
+
 	return_list=[]
-	for hit in hits:
-		doc=searcher.doc(hit.doc)
-		return_list.append(doc.get("$DATA$"))
+	if len(tofind_keyvalue_pairs)>0:		
+		query=lucene.BooleanQuery()
+		for key in tofind_keyvalue_pairs.keys():
+			#add filtering for non primary keys as well###########IMPORTANT
+			if key in primary_keys_map[collection_name]:
+				temp=lucene.QueryParser(lucene.Version.LUCENE_CURRENT,key,analyzer).parse(tofind_keyvalue_pairs[key])
+				query.add(lucene.BooleanClause(temp,lucene.BooleanClause.Occur.MUST))
+		hits=searcher.search(query,MAX_RESULTS).scoreDocs
+		
+		for hit in hits:
+			doc=searcher.doc(hit.doc)
+			return_list.append(doc.get("$DATA$"))
+	else:
+		for i in range(0,searcher.maxDoc()):
+			doc=searcher.doc(i)
+			return_list.append(doc.get("$DATA$"))
+
+	
+
+	
 
 	if len(return_list)==0:
 		return None	
@@ -83,13 +118,15 @@ def number(collection_name):
 		INDEX_DIR=collection_name
 	else:
 		INDEX_DIR=INDEX_DIR_DEFAULT
-	try:	
-		direc=lucene.SimpleFSDirectory(lucene.File(INDEX_DIR))
+		
+	direc=lucene.SimpleFSDirectory(lucene.File(INDEX_DIR))
+  	analyzer=lucene.StandardAnalyzer(lucene.Version.LUCENE_CURRENT)
+  	try:
+  		searcher=lucene.IndexSearcher(direc)
   	except:
   		return 105
-  	analyzer=lucene.StandardAnalyzer(lucene.Version.LUCENE_CURRENT)
-  	searcher=lucene.IndexSearcher(direc)
   	numdocs = int(searcher.maxDoc())
+  	
   	return numdocs
 
 
@@ -99,17 +136,19 @@ if __name__ == "__main__":
 	#####load required resources from metafile ##################
 	print "Initialized lucene with version number :",lucene.VERSION
 	if os.path.exists("metafile.csv"):	
+		
 		f=open("metafile.csv",'rb')
-	    for key, val in csv.reader(f):
-	        primary_keys_map[key]=eval(val)
-	    f.close()
+		for key, val in csv.reader(f):
+			primary_keys_map[key]=eval(val)
+
+		f.close()
 	
 	###use RabbitMQ to handle multiple requests and call appropriate functions
 	###remove this lame if else conditional execution
 	
 	while(True):
 		choice=raw_input("Enter operation to be performed(store,select,number,exit)")
-		if(choice=="store"):
+		if (choice=="store"):
 						collection_name=raw_input("Enter name of the Collection(ENTER \"DEFAULT\" for default table)::")
 						
 						data=raw_input("Enter the data in json format::")
@@ -136,19 +175,24 @@ if __name__ == "__main__":
 						elif SUCCESS_MESSAGE==102:
 							print "Lucene Storage error!"
 							continue
+						elif SUCCESS_MESSAGE==106:
+							print "Record with same primary keys already exists!"
+							continue
 						else:
 							print "error in insertion!"
 							continue
 		elif (choice=="select"):
 						collection_name=raw_input("Enter name of the Collection(ENTER \"DEFAULT\" for default table)::")
+						
 						####################  to be changed as select query  #############################
-						primary_keyvalue_pairs={}
-						for primary_key in primary_keys_map[collection_name]:
-							primary_value=raw_input("Enter the primary key value for "+primary_key+":::")
-							primary_keyvalue_pairs[primary_key]=
+						#tofind_keyvalue_pairs={}
+						#for primary_key in primary_keys_map[collection_name]:
+						#	primary_value=raw_input("Enter the primary key value for "+primary_key+":::")
+						#	tofind_keyvalue_pairs[primary_key]=primary_value
+						tofind=raw_input("Enter key value pairs to search against in JSON format::")
 						####################  to be changed as select query  ##############################	
 						
-						SUCCESS_MESSAGE=search(collection_name,primary_keyvalue_pairs)
+						SUCCESS_MESSAGE=search(collection_name,tofind)
 
 						if (not isinstance(SUCCESS_MESSAGE,int)):
 							if SUCCESS_MESSAGE is None:
@@ -158,13 +202,7 @@ if __name__ == "__main__":
 							continue
 						#later add success/failure codes for denoting what failed
 						elif SUCCESS_MESSAGE==100:
-							print "Check primary_key value!"
-							continue
-						elif SUCCESS_MESSAGE==101:
-							print "Make sure you gave correct primary_keys!"
-							continue
-						elif SUCCESS_MESSAGE==103:
-							print "Lucene Retrieval error!"
+							print "JSON format error!Check input!"
 							continue
 						elif SUCCESS_MESSAGE==105:
 							print "Invalid collection_name!"	
@@ -174,13 +212,18 @@ if __name__ == "__main__":
 		elif (choice=="number"):
 						collection_name=raw_input("Enter name of the Collection(ENTER \"DEFAULT\" for default table)::")
 						SUCCESS_MESSAGE=number(collection_name)
-						print SUCCESS_MESSAGE
+						if SUCCESS_MESSAGE == 105:
+							print "Invalid collection_name!"
+							continue
+						else:	
+							print SUCCESS_MESSAGE
 		elif (choice=="exit"):
-						f=open("metafile.csv","wb");
-						w = csv.writer(f)
-    					for key, val in primary_keys_map.items():
-        					w.writerow([key, val])
-    					f.close()
+						if len(primary_keys_map) > 0:
+							f=open("metafile.csv","wb")
+							w = csv.writer(f)
+							for key, val in primary_keys_map.items():
+								w.writerow([key, val])
+							f.close()
 						break
 
 
@@ -190,5 +233,12 @@ if __name__ == "__main__":
 	
 
 	######################NOTES##############################
-	#1)Make a check condition for primary key
+	
+	#2)Set of existing primary keys for the frequently accessed collection
+	#  can be loaded into p-memory just to prevent checking latency always while inserting.
+	#3)Partial data can be cached for frequently accessed records.
 	#2)Data overriten if same primary_key given
+	#4)Storage can be done in BSON instead of JSON
+	#5)Basic understanding is that we retrieve documents based on some primary_keys and process the returned records for further 
+	#  filtering by parsing the JSON/BSON documents stored
+	#6)IMPORTATNT check comment on line number 93!!	
