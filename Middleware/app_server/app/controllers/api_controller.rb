0d86658a235ec954b29e754c27dc4054e35b8b66
Nitin Jamadagni <nitin.jamadagni@gmail.com>
2014-11-03 23:56:20 +0530

    Updated new requests and handled requests with db server

--- Middleware/app_server/app/controllers/api_controller.rb
+++ Middleware/app_server/app/controllers/api_controller.rb
@@ -1,46 +1,58 @@
 class ApiController < ApplicationController
 	def insert
 		#sample api query
-		#/api/insert?collection=<collection_name>&values=[attr1:val1,attr2:val2]
+		#/api/insert?collection=<collection_name>&values=[attr1:val1,attr2:val2]&primary_keys="key1,key2"&compressed=True|true|false
 
 		collection=params[:collection]
 		values=params[:values]
 		puts collection
 		values[0]='{'
 		values[values.length-1]='}'
-		values_json=JSON.parse(values)
-		#puts values_json["one"]
+		values.gsub! '\"','"'
+		values.gsub! '"','\"'
+		values.gsub! '\",\"','\"\,\"'
+		puts values
+		primary_keys=params[:primary_keys]
+		primary_keys.gsub! '"',''
+		puts primary_keys
+		compressed=params[:compressed]
+		puts compressed
 
-		# call the necessary method from Backend with collection & values hash
+		# call the necessary method from Backend with collection & conditions hash
 		# get output and return as json to api client
+		if primary_keys!=''
+			display_output='python /home/nitin/Desktop/DATABASE-PROJECT/send.py '+collection+' store '+values+' '+primary_keys+' '+compressed
+		else
+			display_output='python /home/nitin/Desktop/DATABASE-PROJECT/send.py '+collection+' store '+values
+		end	
+		return_output = `#{display_output}`
+		response_json = {'response'=>return_output}.to_json
 
-		render json: values_json
+		render json: response_json
 	end
 
 	def display
 		#sample api query
-		#/api/display?collection=<collection_name>&conditions=[attr1:val1,attr2:val2]
+		#/api/display?collection=<collection_name>&values=[attr1:val1,attr2:val2]
 
 		collection=params[:collection]
-		conditions=params[:conditions]
+		values=params[:values]
 		puts collection
-		conditions[0]='{'
-		conditions[conditions.length-1]='}'
-		conditions_json=JSON.parse(conditions)
-		conditions.gsub! '\"','"'
-		conditions.gsub! '"','\"'
-		conditions.gsub! '\",\"','\"\,\"'
-		puts conditions
+		values[0]='{'
+		values[values.length-1]='}'
+		values.gsub! '\"','"'
+		values.gsub! '"','\"'
+		values.gsub! '\",\"','\"\,\"'
+		puts values
 
 		# call the necessary method from Backend with collection & conditions hash
 		# get output and return as json to api client
 
-		display_output='python /home/nitin/Desktop/DATABASE-PROJECT/send.py '+collection+' select '+conditions
-		# `#{display_output}`
-		# temp='echo hello'
-		puts `#{display_output}`
+		display_output='python /home/nitin/Desktop/DATABASE-PROJECT/send.py '+collection+' select '+values
+		return_output = `#{display_output}`
+		response_json = {'response'=>return_output}.to_json
 
-		render json: conditions_json
+		render json: response_json
 	end
 
 	def modify
@@ -53,32 +65,52 @@ class ApiController < ApplicationController
 		puts collection
 		conditions[0]='{'
 		conditions[conditions.length-1]='}'
-		conditions_json=JSON.parse(conditions)
+		conditions.gsub! '\"','"'
+		conditions.gsub! '"','\"'
+		conditions.gsub! '\",\"','\"\,\"'
+		puts conditions
+
+
 		values[0]='{'
 		values[values.length-1]='}'
-		values_json=JSON.parse(values)
-
-		# call the necessary method from Backend with collection & values & conditions hash
-		# get output and return as json to api client
-
-		render json: values_json
-	end
-
-	def delete
-		#sample api query
-		#/api/delete?collection=<collection_name>&conditions=[attr1:val1,attr2:val2]
-
-		collection=params[:collection]
-		conditions=params[:conditions]
-		puts collection
-		conditions[0]='{'
-		conditions[conditions.length-1]='}'
-		conditions_json=JSON.parse(conditions)
+		values.gsub! '\"','"'
+		values.gsub! '"','\"'
+		values.gsub! '\",\"','\"\,\"'
+		puts values
+		
 
 		# call the necessary method from Backend with collection & conditions hash
 		# get output and return as json to api client
 
-		render json: conditions_json
+		display_output='python /home/nitin/Desktop/DATABASE-PROJECT/send.py '+collection+' update '+conditions+' '+values
+		return_output = `#{display_output}`
+		response_json = {'response'=>return_output}.to_json
+
+		render json: response_json
+	end
+
+	def delete
+		#sample api query
+		#/api/delete?collection=<collection_name>&values=[attr1:val1,attr2:val2]
+
+		collection=params[:collection]
+		values=params[:values]
+		puts collection
+		values[0]='{'
+		values[values.length-1]='}'
+		values.gsub! '\"','"'
+		values.gsub! '"','\"'
+		values.gsub! '\",\"','\"\,\"'
+		puts values
+
+		# call the necessary method from Backend with collection & conditions hash
+		# get output and return as json to api client
+
+		display_output='python /home/nitin/Desktop/DATABASE-PROJECT/send.py '+collection+' delete '+values
+		return_output = `#{display_output}`
+		response_json = {'response'=>return_output}.to_json
+
+		render json: response_json
 	end
 
 end
